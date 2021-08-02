929788d8687b38607679e76d5582065530cf897b
babyazalea <usun12@hotmail.com>
2021-08-02 17:27:47 +0900

    added make-up-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MakeUpComponent } from './make-up/make-up.component';
 import { HeaderComponent } from './ui/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, MakeUpComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
