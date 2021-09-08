3f9e77a30e981b5b60f5256dbdaa48e4d39948a1
babyazalea <usun12@hotmail.com>
2021-09-08 14:22:35 +0900

    new layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MakeUpComponent } from './make-up/make-up.component';
-import { HeaderComponent } from './ui/header/header.component';
+import { NavigationComponent } from './ui/navigation/navigation.component';
+import { PlateComponent } from './plate/plate.component';
+import { ListBoxComponent } from './list-box/list-box.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, MakeUpComponent],
+  declarations: [
+    AppComponent,
+    NavigationComponent,
+    MakeUpComponent,
+    PlateComponent,
+    ListBoxComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/list-box/list-box.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-box',
+  templateUrl: './list-box.component.html',
+  styleUrls: ['./list-box.component.css'],
+})
+export class ListBoxComponent {}
