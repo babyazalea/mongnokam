f25f0811ee3d06ece9b6212b775ea59b4f7f73c0
babyazalea <usun12@hotmail.com>
2021-11-07 03:48:42 +0900

    github login test on nav

--- src/app/ui/navigation/navigation.component.ts
+++ src/app/ui/navigation/navigation.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
 
+import { environment } from 'src/environments/environment';
+
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css'],
 })
-export class NavigationComponent {}
+export class NavigationComponent {
+  githubLoginConfig = {
+    client_id: environment.githubApiKey,
+    allow_signup: 'false',
+    scope: 'repo' + ' ' + 'user',
+  };
+
+  githubLoginConfigUrl = new URLSearchParams(this.githubLoginConfig).toString();
+
+  githubLoginUrl = `https://github.com/login/oauth/authorize?client_id=${environment.githubApiKey}&allow_signup=false&scope=user%20repo`;
+}
