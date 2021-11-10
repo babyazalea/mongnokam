0dc760b0174d8883c7cecc452d179f8da4a5dc66
babyazalea <usun12@hotmail.com>
2021-11-11 00:28:34 +0900

    success getting access token from github rest api

--- src/app/ui/navigation/navigation.component.ts
+++ src/app/ui/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 import { signInWithPopup, GithubAuthProvider, Auth } from '@angular/fire/auth';
 
@@ -10,15 +11,40 @@ import { signInWithPopup, GithubAuthProvider, Auth } from '@angular/fire/auth';
 export class NavigationComponent {
   constructor(
     public auth: Auth,
-    public githubAuthProvider: GithubAuthProvider
+    public githubAuthProvider: GithubAuthProvider,
+    public http: HttpClient
   ) {}
 
   githubLoginHandler() {
-    signInWithPopup(this.auth, this.githubAuthProvider)
+    const ghProvider = this.githubAuthProvider;
+
+    ghProvider.addScope('repo user');
+    ghProvider.setCustomParameters({
+      allow_signup: 'false',
+    });
+
+    signInWithPopup(this.auth, ghProvider)
       .then((result) => {
         const credential = GithubAuthProvider.credentialFromResult(result);
         const token = credential?.accessToken;
-        console.log(token);
+        if (token) {
+          localStorage.setItem('accessToken', token);
+
+          this.http
+            .get('https://api.github.com/user', {
+              headers: {
+                Authorization: `token ${token}`,
+              },
+            })
+            .subscribe(
+              (response: any) => {
+                const res = response;
+                console.log(res['public_repos']);
+                console.log(res['total_private_repos']);
+              },
+              (error) => console.log(error)
+            );
+        }
       })
       .catch((error) => console.log(error));
   }
