8dac59c4bcb4eca18596771359ef49d088d9f78f
babyazalea <usun12@hotmail.com>
2021-11-13 00:23:36 +0900

    getting user info

--- src/app/shared/user/user.service.ts
+++ src/app/shared/user/user.service.ts
@@ -1,12 +1,28 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
   // user variable
-  private user!: { username: string; avatarUrl: string; githubUrl: string };
+  private user!: {
+    username: string;
+    avatarUrl: string;
+    githubUrl: string;
+    publicRepoAmount: number;
+    privateRepoAmount: number;
+  };
+  private userUpdated = new Subject<{
+    user: {
+      username: string;
+      avatarUrl: string;
+      githubUrl: string;
+      publicRepoAmount: number;
+      privateRepoAmount: number;
+    };
+  }>();
 
   constructor(private http: HttpClient) {}
 
@@ -15,11 +31,24 @@ export class UserService {
       .get('https://api.github.com/user', {
         headers: {
           Authorization: `token ${token}`,
+          'Content-Type': 'application/json',
         },
       })
       .subscribe(
         (response: any) => {
-          console.log(response);
+          if (response.login) {
+            const loggedInUser = {
+              username: response.login,
+              avatarUrl: response['avatar_url'],
+              githubUrl: response['html_url'],
+              publicRepoAmount: response['public_repos'],
+              privateRepoAmount: response['owned_private_repos'],
+            };
+            this.user = loggedInUser;
+            this.userUpdated.next({
+              user: this.user,
+            });
+          }
         },
         (error) => console.log(error)
       );
