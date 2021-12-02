d418933c40996dc972678d5a9f4b0100d4990480
babyazalea <usun12@hotmail.com>
2021-12-03 01:09:24 +0900

    cotrol alert-bar by root-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { AuthService } from './shared/auth/auth.service';
+import { AlertBarService } from './ui/alert-bar/alert-bar.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'mongnokam';
 
-  constructor(private authService: AuthService) {}
+  // alert state
+  isAlert: boolean = false;
+  alertMessage!: string;
+  alertSub!: Subscription;
+
+  constructor(
+    private authService: AuthService,
+    private alertBarService: AlertBarService
+  ) {}
 
   ngOnInit() {
     this.authService.autoAuth();
+    this.alertMessage = this.alertBarService.getAlert();
+    this.alertSub = this.alertBarService
+      .alertMessageListener()
+      .subscribe((alertData) => {
+        if (alertData.message !== '') {
+          this.isAlert = true;
+        }
+        this.alertMessage = alertData.message;
+      });
+  }
+
+  clearAlert() {
+    this.isAlert = false;
+    this.alertBarService.clearAlert();
+  }
+
+  ngOnDestroy() {
+    this.alertSub.unsubscribe();
   }
 }
--- src/app/ui/alert-bar/alert-bar.component.ts
+++ src/app/ui/alert-bar/alert-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import {
   state,
   style,
@@ -6,8 +6,6 @@ import {
   transition,
   animate,
 } from '@angular/animations';
-import { Subscription } from 'rxjs';
-import { AlertBarService } from './alert-bar.service';
 
 @Component({
   selector: 'app-alert-bar',
@@ -33,35 +31,16 @@ import { AlertBarService } from './alert-bar.service';
           transform: 'translateY(-100%)',
         })
       ),
-      transition('* => *', [animate('0.2s')]),
+      transition('* => show', [animate('0.2s')]),
+      transition('* => hide', [animate('0s')]),
     ]),
   ],
 })
-export class AlertBarComponent implements OnInit, OnDestroy {
-  isAlert: boolean = false;
-  alertMessage!: string;
-  alertSub!: Subscription;
-
-  constructor(private alertBarService: AlertBarService) {}
-
-  ngOnInit() {
-    this.alertMessage = this.alertBarService.getAlert();
-    this.alertSub = this.alertBarService
-      .alertMessageListener()
-      .subscribe((alertData) => {
-        if (alertData.message !== '') {
-          this.isAlert = true;
-        }
-        this.alertMessage = alertData.message;
-      });
-  }
+export class AlertBarComponent {
+  @Input() alertMessage!: string;
+  @Output() clearAlertEvent: EventEmitter<void> = new EventEmitter();
 
   clearAlertHandler() {
-    this.alertBarService.clearAlert();
-    this.isAlert = false;
-  }
-
-  ngOnDestroy() {
-    this.alertSub.unsubscribe();
+    this.clearAlertEvent.emit();
   }
 }
