0a755901fb93321a5c1514369bc86b3eb3767e74
babyazalea <usun12@hotmail.com>
2021-12-14 09:52:08 +0900

    added get lists in my-lists page

--- src/app/pages/my-lists/my-lists.component.ts
+++ src/app/pages/my-lists/my-lists.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 import { RepoList } from 'src/app/components/repo-lists/repo-list/repo-list.model';
+import { RepoListsService } from 'src/app/components/repo-lists/repo-lists.service';
 
 @Component({
   selector: 'app-my-lists',
   templateUrl: './my-lists.component.html',
   styleUrls: ['./my-lists.component.css', '../../shared/styles/main.css'],
 })
-export class MyListsComponent implements OnInit {
+export class MyListsComponent implements OnInit, OnDestroy {
   myFavoriteLists!: Array<RepoList>;
   myLists!: Array<RepoList>;
 
-  ngOnInit() {}
+  private myListsSub!: Subscription;
+
+  constructor(private repoListsService: RepoListsService) {}
+
+  ngOnInit() {
+    this.myLists = this.repoListsService.getMyLists();
+    this.myListsSub = this.repoListsService
+      .myListsUpdateListener()
+      .subscribe((listsData) => {
+        this.myLists = listsData.lists;
+      });
+  }
+
+  ngOnDestroy(): void {}
 }
