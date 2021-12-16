527b16263383b2671c92059e581e4bc4193cc5e8
babyazalea <usun12@hotmail.com>
2021-12-16 10:59:47 +0900

    change aside when changed route

--- src/app/ui/aside/aside.component.ts
+++ src/app/ui/aside/aside.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 import { RepoListsService } from 'src/app/components/repo-lists/repo-lists.service';
-import { UserService } from 'src/app/shared/user/user.service';
 
 @Component({
   selector: 'app-aside',
@@ -10,13 +11,22 @@ import { UserService } from 'src/app/shared/user/user.service';
   styleUrls: ['./aside.component.css'],
 })
 export class AsideComponent implements OnInit, OnDestroy {
+  isMakeUpPage!: boolean;
   detectingChangingMyList: boolean = false;
   private myListsSub: Subscription = new Subscription();
 
   constructor(
-    private repoListsService: RepoListsService,
-    private userService: UserService
-  ) {}
+    private router: Router,
+    private repoListsService: RepoListsService
+  ) {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event) => {
+        if (event instanceof NavigationEnd) {
+          this.isMakeUpPage = event.url === '/' ? true : false;
+        }
+      });
+  }
 
   ngOnInit() {
     this.myListsSub = this.repoListsService
