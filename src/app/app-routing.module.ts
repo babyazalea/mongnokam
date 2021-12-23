0fe2918c7678fe1a075b682f792ce189b8e40f97
babyazalea <usun12@hotmail.com>
2021-12-23 13:21:41 +0900

    added auth-guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { MakeUpComponent } from './pages/make-up/make-up.component';
 import { MyListsComponent } from './pages/my-lists/my-lists.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import {AuthGuard} from "./shared/auth/auth.guard";
 
 const routes: Routes = [
   { path: '', component: MakeUpComponent },
   {
     path: 'my-lists',
     component: MyListsComponent,
+    canActivate: [AuthGuard]
   },
   { path: '**', component: PageNotFoundComponent },
 ];
